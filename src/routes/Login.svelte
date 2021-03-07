fa6860b2e1c66ade31a453956e5152af344e9662
jjarrett21 <james.jarrett@okstate.edu>
2021-03-06 18:44:18 -0600

    more styles

--- src/routes/Login.svelte
+++ src/routes/Login.svelte
@@ -20,40 +20,52 @@
   <title>Gunpla Reader</title>
 </svelte:head>
 
-<div class="w-screen h-screen py-10 sm:px-2 flex flex col justify-center mt-80">
+<div class="w-screen h-screen py-10 sm:px-2 flex flex col">
   <form on:submit={handleSubmit} class="md:w-1/4 w-5/6">
-    <img
-      class="mx-auto mb-4 h-36 w-auto"
-      src="/gunplaicon.png"
-      alt="Gunpla Reader Logo"
-    />
-    <div class="w-full mb-4">
-      <label for="email" class="cursor-pointer py-2">Email</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        placeholder="you@email.com"
-        required
-        bind:value={email}
-        class="w-full px-2 py-1 rounded-sm text-black"
+    <div class="absolute left-0 top-0">
+      <img
+        class="mx-auto mb-4 h-10 w-auto "
+        src="/gunplaicon.png"
+        alt="Gunpla Reader Logo"
       />
     </div>
-    <div class="w-full mb-6">
-      <label for="password" class="cursor-pointer py-2">Password</label>
-      <input
-        id="password"
-        name="password"
-        type="password"
-        required
-        bind:value={password}
-        class="w-full px-2 py-1 roudned-sm text-black"
-      />
+
+    <div
+      class="absolute w-auto h-full right-0 pr-10 rounded-lg border-yellow-500 shadow-xl"
+    >
+      <!-- svelte-ignore a11y-img-redundant-alt -->
+      <img class="mx-auto w-auto h-auto" src="/27.jpg" alt="side image" />
     </div>
-    <div class="w-full">
-      <Button type="submit" className="inline-flew w-full justify-center"
-        >Login</Button
-      >
+
+    <div class=" pl-30 w-auto left-0 mt-60">
+      <div class="w-full mb-4">
+        <label for="email" class="cursor-pointer py-2">Email</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          placeholder="you@email.com"
+          required
+          bind:value={email}
+          class="w-full px-2 py-1 rounded-sm text-black"
+        />
+      </div>
+      <div class="w-full mb-6">
+        <label for="password" class="cursor-pointer py-2">Password</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          required
+          bind:value={password}
+          class="w-full px-2 py-1 roudned-sm text-black"
+        />
+      </div>
+      <div class="w-full">
+        <Button type="submit" className="inline-flew w-full justify-center"
+          >Login</Button
+        >
+      </div>
     </div>
   </form>
 </div>
