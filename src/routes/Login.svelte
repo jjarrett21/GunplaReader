62b72ff5a2aabcc83aed8088499b29bebfc86dd9
jjarrett21 <james.jarrett@okstate.edu>
2021-03-11 11:47:04 -0600

    big style help with Markie

--- src/routes/Login.svelte
+++ src/routes/Login.svelte
@@ -19,25 +19,19 @@
 <svelte:head>
   <title>Gunpla Reader</title>
 </svelte:head>
+<!-- Container css -->
+<div class="flex justify-between items-stretch h-screen w-auto">
+  <!-- Left window css -->
+  <div class="flex items-center justify-center flex-grow w-1/2 h-screen">
+    <form on:submit={handleSubmit} class="w-1/2">
+      <div class="">
+        <img
+          class="mx-auto mb-4 h-14 w-auto "
+          src="/gunplaicon.png"
+          alt="Gunpla Reader Logo"
+        />
+      </div>
 
-<div class="w-screen h-screen py-10 sm:px-2 flex flex col">
-  <form on:submit={handleSubmit} class="md:w-1/4 w-5/6">
-    <div class="absolute left-0 top-0">
-      <img
-        class="mx-auto mb-4 h-10 w-auto "
-        src="/gunplaicon.png"
-        alt="Gunpla Reader Logo"
-      />
-    </div>
-
-    <div
-      class="absolute w-auto h-full right-0 pr-10 rounded-lg border-yellow-500 shadow-xl"
-    >
-      <!-- svelte-ignore a11y-img-redundant-alt -->
-      <img class="mx-auto w-auto h-auto" src="/27.jpg" alt="side image" />
-    </div>
-
-    <div class=" pl-30 w-auto left-0 mt-60">
       <div class="w-full mb-4">
         <label for="email" class="cursor-pointer py-2">Email</label>
         <input
@@ -47,7 +41,7 @@
           placeholder="you@email.com"
           required
           bind:value={email}
-          class="w-full px-2 py-1 rounded-sm text-black"
+          class="w-full h-12 px-2 py-1 rounded-sm text-black"
         />
       </div>
       <div class="w-full mb-6">
@@ -58,14 +52,34 @@
           type="password"
           required
           bind:value={password}
-          class="w-full px-2 py-1 roudned-sm text-black"
+          class="w-full h-12 px-2 py-1 roudned-sm text-black"
         />
       </div>
       <div class="w-full">
-        <Button type="submit" className="inline-flew w-full justify-center"
+        <Button type="submit" className="inline-flew w-full justify-center h-12"
           >Login</Button
         >
       </div>
-    </div>
-  </form>
+    </form>
+  </div>
+  <!-- Right window css -->
+  <div
+    class="right flex-grow w-1/2 bg-center bg-no-repeat bg-cover"
+    style="background-image:url('/27.jpg')"
+  />
 </div>
+
+<style>
+  .right {
+    position: relative;
+  }
+  .right::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.2);
+  }
+</style>
